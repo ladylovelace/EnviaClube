ea307083c850cb4fb0cd42685a9fa5eca2ea0b06
AdrianoSong <adrianosong@hotmail.com>
2015-08-22 20:48:18 -0300

    first

---
+++ app/build/generated/source/buildConfig/debug/io/fabric/samples/cannonball/BuildConfig.java
@@ -0,0 +1,17 @@
+/**
+ * Automatically generated file. DO NOT MODIFY
+ */
+package io.fabric.samples.cannonball;
+
+public final class BuildConfig {
+  public static final boolean DEBUG = Boolean.parseBoolean("true");
+  public static final String APPLICATION_ID = "io.fabric.samples.cannonball";
+  public static final String BUILD_TYPE = "debug";
+  public static final String FLAVOR = "";
+  public static final int VERSION_CODE = 7;
+  public static final String VERSION_NAME = "1.0.0";
+  // Fields from build type: debug
+  public static final String CONSUMER_KEY = "Ae9QU83SdV04D6XMuYKYMtrxo";
+  public static final String CONSUMER_SECRET = "6hubxthXbVSbUlnOESDl952Hdiqh071aFo6SoEHF0aB5WmAsp4";
+  public static final String MOPUB_AD_UNIT_ID = "EDIT_THIS";
+}
